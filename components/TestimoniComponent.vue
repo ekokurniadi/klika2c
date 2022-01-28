4e0f214abdfc5718e23a4df3fdaa39dc390acae1
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-28 15:16:16 +0700

    first commit

---
+++ components/TestimoniComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12 yellow-section">
+          <h3 class="yellow-section-title">Kisah Peminjam Dana</h3>
+          <p class="yellow-section-subtitle">
+            Kisah-kisah sukses pengguna KlikACC yang berhasil merubah hidupnya
+            dengan KlikACC
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="image-testimoni-group">
+          <div class="image-testimoni-item" v-for="item of testimoniData" :key="item.id">
+              <img
+                :src="'/images/' + item.image_thumbnail"
+                :alt="item.image_thumbnail"
+                class="img-testimoni"
+              />
+              <div class="circle-button-read">
+                <a href="">Baca kisah {{ item.person_name }}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import testimoniDatas from '@/assets/data/testimoni.json'
+export default {
+  data() {
+    return {
+      testimoniData: {},
+    }
+  },
+  mounted() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.testimoniData = testimoniDatas.data
+      console.log(this.testimoniData)
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <app-testimoni-component/>
+</template>
+
+<script>
+import TestimoniComponent from '@/components/TestimoniComponent.vue'
+export default {
+  name: 'IndexPage',
+  components:{
+    'app-testimoni-component':TestimoniComponent,
+  }
+}
+</script>
